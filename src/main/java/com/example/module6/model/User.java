51582796de5d0df23aa260d09815a961738518a3
H95u <hieu.dh3t1@gmail.com>
2023-08-01 18:43:36 +0700

    update model

--- src/main/java/com/example/module6/model/User.java
+++ src/main/java/com/example/module6/model/User.java
@@ -3,6 +3,8 @@ package com.example.module6.model;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -14,6 +16,17 @@ public class User {
     private String username;
     private String password;
     private String email;
+    private String nickname;
+    private String img;
+    private Double price;
+    private Integer gender;
+    private LocalDate dob;
+    private Integer status;
+    private Double money;
+    @ManyToMany
+    private List<Options> options;
+    @ManyToOne
+    private Address address;
 
     @ManyToMany(fetch = FetchType.EAGER)
     private Set<Role> roles;
--- src/main/java/com/example/module6/service/IUserInfoService.java
+++
@@ -1,6 +0,0 @@
-package com.example.module6.service;
-
-import com.example.module6.model.UserInfo;
-
-public interface IUserInfoService extends IGeneralService<UserInfo, Long> {
-}
