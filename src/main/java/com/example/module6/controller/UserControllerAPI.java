70ff8856250b0a05bf8f7cb5faef7ec2883a5456
TranNgocTung <ngoctung10a6@gmail.com>
2023-08-07 09:46:16 +0700

    search_filter

--- src/main/java/com/example/module6/controller/UserControllerAPI.java
+++ src/main/java/com/example/module6/controller/UserControllerAPI.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -83,4 +84,36 @@ public class UserControllerAPI {
         return new ResponseEntity<>(tours, HttpStatus.OK);
     }
 
+    @Autowired
+    private IUserRepository userRepository;
+
+    @GetMapping("/filter")
+    public List<User> getUsers(
+            @RequestParam(name = "gender", required = false) Integer gender,
+            @RequestParam(name = "address", required = false) Long addressId,
+            @RequestParam(name = "viewCount", required = false) Long viewCount,
+            @RequestParam(name = "rentCount", required = false) Long rentCount,
+            @RequestParam(name = "minAge", required = false) Integer minAge,
+            @RequestParam(name = "maxAge", required = false) Integer maxAge,
+            @RequestParam(name = "username", required = false) String username
+    ) {
+        if (gender == null || addressId == null || viewCount == null || rentCount == null || minAge == null || maxAge == null || username == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Vui lòng nhập đầy đủ các trường tìm kiếm.");
+        }
+
+        List<User> users = userRepository.findAllByGenderAndAddressIdAndViewCountAndRentCountAndAgeBetweenAndUsernameContainingIgnoreCase(
+                gender,
+                addressId,
+                viewCount,
+                rentCount,
+                minAge,
+                maxAge,
+                username
+        );
+
+        return users;
+    }
+
 }
+
+
