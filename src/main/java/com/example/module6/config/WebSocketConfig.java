bb332dbcf91bf855ba0779d60d6f9598a23cb15b
H95u <hieu.dh3t1@gmail.com>
2023-08-09 00:19:16 +0700

    update chat

--- src/main/java/com/example/module6/config/WebSocketConfig.java
+++ src/main/java/com/example/module6/config/WebSocketConfig.java
@@ -1,22 +1,37 @@
 package com.example.module6.config;
 
+import com.example.module6.model.Message;
+import com.example.module6.service.impl.MessageService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.time.LocalDateTime;
 
 @Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
-    }
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+    @Autowired
+    private MessageService messageService;
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic","queue");
-        registry.setApplicationDestinationPrefixes("/app");
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new TextWebSocketHandler() {
+            @Override
+            protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+                String payload = message.getPayload();
+                ObjectMapper objectMapper = new ObjectMapper();
+                Message newMessage = objectMapper.readValue(payload, Message.class);
+                newMessage.setTimestamp(LocalDateTime.now());
+                messageService.save(newMessage);
+                session.sendMessage(new TextMessage("Processed: " + payload));
+            }
+        }, "/api/message").setAllowedOrigins("*");
     }
 }
