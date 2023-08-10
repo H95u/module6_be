b81cd9957a6b3d8efcf2ddb43de1c2ce3ef7d458
Lam <lamvi1522@gmail.com>
2023-08-10 20:46:51 +0700

    firstcommit

--- src/main/java/com/example/module6/controller/FeedbackController.java
+++ src/main/java/com/example/module6/controller/FeedbackController.java
@@ -2,11 +2,13 @@ package com.example.module6.controller;
 
 import com.example.module6.model.Feedback;
 import com.example.module6.service.impl.FeedbackService;
+import jdk.vm.ci.meta.Local;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -23,6 +25,7 @@ public class FeedbackController {
 
     @PostMapping("/create")
     public ResponseEntity<Feedback> createFeedback(@RequestBody Feedback feedback) {
+        feedback.setPresentTime(LocalDateTime.now());
         Feedback createdFeedback = feedbackService.createFeedback(feedback);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdFeedback);
     }
