79a4fa00e1b622a4e2fc75fe824e1149ca240e54
Lam <lamvi1522@gmail.com>
2023-08-11 17:25:48 +0700

    update-sideber-image

--- src/main/java/com/example/module6/model/Options.java
+++ src/main/java/com/example/module6/model/Options.java
@@ -13,6 +13,7 @@ public class Options {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String image;
     private String name;
     private double price;
 }
